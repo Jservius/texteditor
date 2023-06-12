8ae14d4fe19fdef983894f4f88b0fd616af2c7e6
jservius <juniorservius@gmail.com>
2023-06-12 16:55:56 -0400

    first commit

---
+++ client/src/js/database.js
@@ -0,0 +1,84 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+
+  console.log('PUT to the database');
+
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+   // Use the .put() method to add data in the database.
+  const request = store.put({ id: 1, todo: content });
+
+   // Get confirmation of the request.
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  
+    console.log('GET from the database');
+
+    // Create a connection to the database database and version we want to use.
+    const jateDb = await openDB('jate', 1);
+
+    // Create a new transaction and specify the database and data privileges.
+    const tx = jateDb.transaction('jate', 'readonly');
+
+    // Open up the desired object store.
+    const store = tx.objectStore('jate');
+
+    // Use the .getAll() method to get all data in the database.
+    const request = store.get(1);
+
+    // Get confirmation of the request.
+    const result = await request;
+    console.log('result.value', result);
+    return result.todo;
+ 
+};
+
+export const deleteDb = async (id) => {
+  console.log('DELETE from the database', id);
+
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('todos', 1);
+
+   // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('todos', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('todos');
+
+  // Use the .delete() method to remove specific data in the database.
+  const request = store.delete(id);
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
+
+initdb();
